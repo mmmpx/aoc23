0cf2c85c846d4c2cde935760f4b47a0b10e56c50
Matteo Paramatti <paramattimatteo@gmail.com>
2023-12-14 22:18:21 +0000

    Day 7 (b)

---
+++ 07b/Main.hs
@@ -0,0 +1,79 @@
+module Main where
+
+import Data.Function
+import Data.List
+import Data.Maybe
+import Data.Ord
+
+newtype Card = Card Int deriving (Eq, Ord)
+
+cardChars :: [Char]
+cardChars = "AKQT98765432J"
+
+card :: Char -> Card
+card c
+  | Just i <- (elemIndex c cardChars) = Card (length cardChars - i - 1)
+  | otherwise = error "invalid card char provided"
+
+instance Show Card where
+  show (Card i) = [cardChars !! (length cardChars - i - 1)]
+
+newtype Hand = Hand [Card]
+
+newtype HandType = HandType Int deriving (Eq, Ord, Show)
+
+hand :: [Card] -> Hand
+hand cs
+  | length cs == 5 = Hand cs
+  | otherwise = error "invalid number of cards provided"
+
+hand' :: [Char] -> Hand
+hand' = hand . map card
+
+recl :: ([a] -> ([b], [a])) -> [a] -> [[b]]
+recl f = fix (\rec xs -> if null xs then [] else let (a,b) = f xs in a:(rec b))
+
+splits :: Hand -> [[Card]]
+splits (Hand cs) = recl (\xs -> span (== head xs) xs) $ sort cs
+
+counts :: Hand -> [(Card, Int)]
+counts = map (\cs -> (head cs, length cs)) . splits
+
+jcounts :: Hand -> [(Card, Int)]
+jcounts h
+  = let (js, cs) = partition ((== card 'J') . fst) $ sort $ counts h
+        j = sum $ map snd js
+    in case cs of
+      [] -> [(card 'J', j)]
+      _  -> let mx = maximum $ map snd cs
+                mxcs = filter ((== mx) . snd) cs
+                mxc = maximum $ map fst mxcs
+                rst = filter ((/= mxc) . fst) cs
+            in rst ++ [(mxc, mx+j)]
+
+ranks :: [([Int], HandType)]
+ranks = zip [ [1,1,1,1,1], [1,1,1,2], [1,2,2], [1,1,3], [2,3], [1,4], [5] ] $ map HandType [0..]
+
+rank :: Hand -> HandType
+rank = fromJust . flip lookup ranks . (sort . map snd . jcounts)
+
+instance Eq Hand where
+  h1@(Hand xs) == h2@(Hand ys) = rank h1 == rank h2 && xs == ys
+
+instance Ord Hand where
+  h1@(Hand xs) `compare` h2@(Hand ys) = (rank h1 `compare` rank h2) <> (xs `compare` ys)
+
+instance Show Hand where
+  show (Hand xs) = concat $ map show xs
+
+type Bid = Int
+
+parse :: [Char] -> (Hand, Bid)
+parse s = let ws = words s in (hand' $ ws !! 0, read $ ws !! 1)
+
+solve :: [(Hand, Bid)] -> Int
+solve = sum . zipWith (*) [1..] . map snd . sortBy (comparing fst)
+
+main :: IO ()
+main = (solve <$> map parse <$> lines <$> getContents) >>= print
+
