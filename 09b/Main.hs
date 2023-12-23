5eddb79b8572cf4bc25843017d739bdb803e6a1d
Matteo Paramatti <paramattimatteo@gmail.com>
2023-12-23 10:49:48 +0000

    Day 9 (b)

---
+++ 09b/Main.hs
@@ -0,0 +1,21 @@
+module Main where
+
+diffs :: [Int] -> [Int]
+diffs [] = []
+diffs [_] = []
+diffs (x:y:zs) = (y-x):(diffs (y:zs))
+
+predict :: [Int] -> Int
+predict xs
+  | all (== 0) xs = 0
+  | otherwise = (head xs) - (predict $ diffs xs)
+
+solve :: [[Int]] -> Int
+solve = sum . map predict
+
+parse :: [Char] -> [[Int]]
+parse = map (map read) . map words . lines
+
+main :: IO ()
+main = (solve . parse <$> getContents) >>= print
+
